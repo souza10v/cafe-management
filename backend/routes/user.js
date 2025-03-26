ad443c646087de71c198c8b22e72914f372371ee
Donizetti de Souza <donizetti.souza9@gmail.com>
2025-03-25 22:50:11 -0300

    feature: endpoint login

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,51 +1,96 @@
 const express = require('express');
-const prisma = require('@prisma/client'); // Importando o Prisma Client
-const bcrypt = require('bcryptjs'); // Importando o bcryptjs para criptografar a senha
+const prisma = require('@prisma/client');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
-// Criar uma instância do Prisma Client
 const { PrismaClient } = prisma;
 const prismaClient = new PrismaClient();
 
 router.post('/signup', async (req, res) => {
-  const { name, email, phone, password, status, role } = req.body;
+    const { name, email, phone, password, status, role } = req.body;
 
-  if (!name || !email || !phone || !password || !status || !role) {
-    return res.status(400).json({ error: 'Todos os campos são obrigatórios' });
-  }
-
-  try {
-    // Verifica se já existe um usuário com o mesmo e-mail
-    const existingUser = await prismaClient.user.findUnique({
-      where: {
-        email: email,
-      },
-    });
-
-    if (existingUser) {
-      return res.status(400).json({ error: 'E-mail já está em uso' });
+    if (!name || !email || !phone || !password || !status || !role) {
+        return res.status(400).json({ error: 'Todos os campos são obrigatórios' });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10); 
+    try {
+        const existingUser = await prismaClient.user.findUnique({
+            where: {
+                email: email,
+            },
+        });
 
-    const user = await prismaClient.user.create({
-      data: {
-        name,
-        email,
-        phone,
-        password: hashedPassword, 
-        status,
-        role,
-      },
-    });
+        if (existingUser) {
+            return res.status(400).json({ error: 'E-mail já está em uso' });
+        }
 
-    res.status(201).json(user);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Erro ao criar o usuário' });
-  } finally {
-    await prismaClient.$disconnect();
-  }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await prismaClient.user.create({
+            data: {
+                name,
+                email,
+                phone,
+                password: hashedPassword,
+                status,
+                role,
+            },
+        });
+
+        res.status(201).json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro ao criar o usuário' });
+    } finally {
+        await prismaClient.$disconnect();
+    }
 });
+
+router.post('/login', async (req, res) => {
+
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(401).json({ error: 'E-mail e senha são obrigatórios' });
+    }
+
+    try {
+        const existingUser = await prismaClient.user.findUnique({
+            where: {
+                email: email,
+            },
+        });
+
+        if (!existingUser) {
+            return res.status(401).json({ error: 'Usuário ou senha inválidos' });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, existingUser.password);
+
+        if (!passwordMatch) {
+            return res.status(401).json({ error: 'Usuário ou senha inválidos' });
+        }
+
+        if (existingUser.status === false) {
+            return res.status(401).json({ error: 'Usuário não está ativo' });
+        }
+
+        const token = jwt.sign(
+            { email: existingUser.email, role: existingUser.role },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        );
+
+        res.json({ message: 'Login realizado com sucesso!', token });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro ao fazer login' });
+    } finally {
+        await prismaClient.$disconnect();
+    }
+
+})
 
 module.exports = router;
